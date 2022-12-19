b289a29a2c2a0598078b2bb2d08f6b2ac7e77635
行事光 <h.gyoji.vrds@gmail.com>
2022-12-19 15:32:56 +0900

    add Wrapper

--- code/01-starting-project/src/components/Users/AddUser.js
+++ code/01-starting-project/src/components/Users/AddUser.js
@@ -5,6 +5,9 @@ import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
+import { Wrapper } from '../Helpers/Wrapper';
+
+
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
@@ -44,7 +47,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +74,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
